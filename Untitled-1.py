644c7dd481cc7ae7bee4a5220c38c3a6f81d8b52
Shakib24345 <sks268335@gmail.com>
2025-07-14 17:13:32 +0600

    1st

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+"""def my_funtion():
+    print("Hello World")
+my_funtion()
+
+def my_function(name): 
+     print("name " + name) 
+my_function("Mike") 
+my_function("Monica") 
+my_function("John")
+
+def my_function(fname, lname): 
+   print(fname + " " + lname) 
+my_function("Emil", "Refsnes")"""
+
+def my_function(*names): 
+   print("Name= " + names[2]) 
+my_function("Mike", "Monica", "John") 
